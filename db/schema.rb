dfdcfb3233b5ac73a551086a89f85204c3aa11c1
Raphaël Saint-Alme <raphael_35_974@outlook.fr>
2024-06-27 14:30:34 +0200

    removed useless password attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_121244) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_122826) do
   create_table "buyers", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_121244) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
